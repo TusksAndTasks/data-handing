b91223392ed79a61f89542c6567940b4a3c9200b
Alexandr Klykov <a.klykov@worksolutions.ru>
2023-09-15 21:50:11 +0300

    Добавлен роутинг

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { RouterProvider } from "react-router";
 import ReactDOM from "react-dom/client";
 
+import { browserRouter } from "./shared/config";
 import { requestManagerHook } from "./shared/hooks";
+import { PageSpin } from "./shared/ui/pageSpin";
 import "./index.css";
 
 requestManagerHook();
@@ -9,6 +12,6 @@ requestManagerHook();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div></div>
+    <RouterProvider router={browserRouter} fallbackElement={<PageSpin />} />
   </React.StrictMode>,
 );
